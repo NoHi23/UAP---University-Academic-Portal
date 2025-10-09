c2ea816b61ad729c2f8f3bc977d10a00f6420c9e
hieunn-dev <nguyenngochieu0323@gmail.com>
2025-10-09 22:15:32 +0700

    hieunn

--- BACKEND/src/routes/student.js
+++ BACKEND/src/routes/student.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { verifyToken, authorize } = require('../middleware/authorization');
 
 const { getStudentMaterials } = require('../controllers/material');
-const { createPaymentUrl, getTransactionHistory } = require('../controllers/paymentController');
+const { createPaymentUrl, getTransactionHistory, getTuitionInfo } = require('../controllers/paymentController');
 const { submitRequest, getMyRequests } = require('../controllers/requestController');
 const { getEvaluableClasses, submitEvaluation } = require('../controllers/evaluationController');
 const { getMySlotNotifications } = require('../controllers/notificationController');
@@ -12,6 +12,7 @@ router.use(verifyToken, authorize('student'));
 
 router.get('/materials/me', getStudentMaterials);
 
+router.get('/tuition/me', getTuitionInfo); 
 router.post('/tuition/create-payment-url', createPaymentUrl);
 router.get('/transactions/me', getTransactionHistory);
 
