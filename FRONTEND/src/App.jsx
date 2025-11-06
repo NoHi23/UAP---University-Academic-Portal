fffeb8b8150459be0a040b8d2facd6c534d883dd
TuanAnhNguyen281 <ntanh2811@gmail.com>
2025-11-06 17:57:45 +0700

    update

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -37,7 +37,7 @@ import EnterGrades from './pages/Lecturer/EnterGrades';
 import StudentGrades from './pages/Lecturer/StudentGrades';
 import PayTuitionPage from './pages/Student/PayTuitionPage';
 import TransactionHistoryPage from './pages/Student/TransactionHistoryPage';
-import RequestsPage from './pages/Student/RequestsPage';
+import RequestsPage from './pages/Student/SupportRequest/RequestsPage';
 import EvaluationPage from './pages/Student/EvaluationPage';
 import SlotNotificationsPage from './pages/Student/SlotNotificationsPage';
 import LecturerAnnouncements from './pages/Lecturer/ViewAnoucement/AnnoucementList';
@@ -64,7 +64,7 @@ import AbsenceList from "./pages/Staff/AbsenceList";
 import AbsenceReview from "./pages/Staff/AbsenceReview";
 import AttendanceList from './pages/Lecturer/AttendanceListPages/AttendanceList';
 import ClassesBySemesterPage from './pages/Lecturer/ClassesBySemester/Index';
-import SupportRequestPage from './pages/Lecturer/SupportRequest';
+import SupportListLecturer from './pages/Lecturer/SupportRequest/SupportList';
 
 
 import ManualClassPage from './pages/Staff/ManualClassPage';
@@ -107,7 +107,7 @@ function App() {
                     <Route path="materials" element={<MaterialsPage />} />
                     <Route path="payment" element={<PayTuitionPage />} />
                     <Route path="transactions" element={<TransactionHistoryPage />} />
-                    <Route path="requests" element={<RequestsPage />} />
+                    <Route path="requests" element={<SupportListLecturer />} />
                     <Route path="evaluation" element={<EvaluationPage />} />
                     <Route path="notifications" element={<SlotNotificationsPage />} />
                     <Route path="chat" element={<AiChatPage />} />
@@ -122,7 +122,7 @@ function App() {
                   <Route path="/student/materials" element={<MaterialsPage />} />
                   <Route path="/student/payment" element={<PayTuitionPage />} />
                   <Route path="/student/transactions" element={<TransactionHistoryPage />} />
-                  <Route path="/student/requests" element={<RequestsPage />} />
+                  <Route path="/student/requests" element={<SupportListLecturer />} />
                   <Route path="/student/evaluation" element={<EvaluationPage />} />
                   <Route path="/student/notifications" element={<SlotNotificationsPage />} />
                   <Route path="/student/schedule" element={<StudentTimetablePage />} />
@@ -169,7 +169,7 @@ function App() {
                   <Route path="announcements" element={<LecturerAnnouncements />} />
                   <Route path='my-list-class-charge' element={<ClassesBySemesterPage />} />
                   <Route path="announcements/:id" element={<LecturerAnnouncementDetail />} />
-                  <Route path="supports" element={<SupportRequestPage />} />
+                  <Route path="supports" element={<SupportListLecturer />} />
                   <Route path="schedule" element={<LecturerTimetablePage />} />
                   <Route path="attendance/:scheduleId" element={<AttendancePage />} />
                   <Route path="view-attendance-list" element={<AttendanceList />} />
